7a2a27b46c6dcc9c57632d7065b879b08dc133f4
Ahmed Nouria <ahmnouira@gmail.com>
2024-01-21 23:00:56 +0100

    feat: added css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
     <title>@yield('title', 'Online Store')</title>
 </head>
 
@@ -37,6 +38,18 @@
     <div class="container my-4">
         @yield('content')
     </div>
+    <!-- footer -->
+    <div class="copyright py-4 text-center text-white">
+        <div class="container">
+            <small>
+                Copyright - <a class="text-reset fw-bold text-decoration none" target="_blank" href="https://twitter.com/ahmnouira">
+                    ahmnouira
+                </a>
+                - <b>Ahmed Nouira</b>
+            </small>
+        </div>
+    </div>
+    <!-- footer -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
 
