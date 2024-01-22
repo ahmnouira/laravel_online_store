15f06b41b9c8e8ea73ed698fdd33e3fd6b1b21b6
Ahmed Nouria <ahmnouira@gmail.com>
2024-01-22 20:38:27 +0100

    feat: added index and about pages

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $viewData = [];
+    $viewData['title'] = "Home Page - Online Store";
+    return view('home.index')->with("viewData", $viewData);
+});
+
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
